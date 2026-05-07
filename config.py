9d99e04a251da70bb4a3da3c8dda10f6e79c7272
liuyijie <liuyijie@qq.com>
2026-05-07 16:02:05 +0800

    5.08 增加整局上传模式

--- config.py
+++ config.py
@@ -20,3 +20,15 @@ class Config:
     FLASK_HOST = "127.0.0.1"
     FLASK_PORT = 5000
     SECRET_KEY = "alpha-games-secret-key"
+
+    XUNFEI_APPID = "914a5257"
+    XUNFEI_API_KEY = "dbe4766077d66c698524cd1314cf2c3f"
+    XUNFEI_API_SECRET = "YWE4NWRjMzg5OWRiZWU4ZWNjMmEwMTlh"
+    XUNFEI_STT_URL = "wss://ws-api.xfyun.cn/v2/iat"
+
+    XUNFEI_LLM_APPID = "914a5257"
+    XUNFEI_LLM_URL = "wss://spark-api.xf-yun.com/v1.1/chat"
+
+    ELEVENLABS_API_KEY = "sk_e638cbf8d1ca252e249fe97ea881e0653a84e3219f22ddeb"
+    ELEVENLABS_VOICE_ID = "sIirIlcPrAxbvQM9Icwj"
+    ELEVENLABS_TTS_URL = "https://api.elevenlabs.io/v1/text-to-speech/{voice_id}/stream"
